66ac8b18b466b5baf493253e355a834eee49a805
psusambriz <sambriz@pdx.edu>
2025-04-28 00:56:59 -0700

    Progress 1

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+// axum
+use axum::{
+    routing::get,
+    Router,
+    response::Html,
+};
+use askama::Template;
+use std::net::SocketAddr;
+
+// html
+#[derive(Template)]
+#[template(path = "quote.html")]
+struct QuoteTemplate {
+    quote: &'static str,
+}
+
+// quote handler
+async fn quote_handler() -> Html<String> {
+    let template = QuoteTemplate {
+        quote: "It's so easy to laugh it's so easy to hate. It takes guts to be gentle and kind.",
+    };
+    Html(template.render().unwrap())
+}
+
+// main 
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(quote_handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    println!("Listening on http://{}", addr);
+
+    axum::serve(
+        tokio::net::TcpListener::bind(addr).await.unwrap(),
+        app.into_make_service(),
+    )
+        .await
+        .unwrap();
+}
